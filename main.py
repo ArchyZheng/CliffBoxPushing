6488e6b5f1c30a2ae3ad75742dcd81b9921b235a
Archy, Continental <zhen0144@e.ntu.edu.sg>
2023-09-26 15:01:03 +0800

    update

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ from omni.isaac.examples.base_sample import BaseSample
 from omni.isaac.core import World
 import numpy as np
 
+"""
+This class is created for creating multitasks in Isaac Sim.
 
+The main method is on_play.
+"""
 class Main(BehaviorScript):
     def on_init(self):
-        print("hello world!")
         self._world = World()
         self._world.clear()
         self._task = []
-        self._number_of_tasks = 12
+        self._number_of_tasks_colomn = 12
+        self._number_of_tasks_row = 6
 
 
     
     def on_play(self):
         self._world.clear()
         self._world.scene.add_default_ground_plane()
-        for i in range(self._number_of_tasks):
-            self._world.add_task(Environment(name="task" + str(i), offset=np.array([0, -40 + 7 * i, 0])))
+        
+        gap_colomn = 7
+        gap_row = 15
+        for colomn in range(self._number_of_tasks_colomn):
+            for row in range(self._number_of_tasks_row):
+                self._world.add_task(Environment(name="task" + "colomn" + str(colomn) + "row" + str(row), offset=np.array([-40 + gap_row * row, -40 + gap_colomn * colomn, 0])))
         return 
